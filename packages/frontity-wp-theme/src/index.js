08db0187572443d3d84b9d244a03dc1ef3323774
Erik Jonsberg <erikj@hungryghostdesign.com>
2021-10-11 16:07:07 -0400

    added index component

---
+++ packages/frontity-wp-theme/src/index.js
@@ -0,0 +1,18 @@
+import Root from "./components";
+
+const FrontityWpTheme = {
+  name: "frontity-wp-theme",
+  roots: {
+    theme: Root,
+  },
+  state: {
+    theme: {},
+  },
+  actions: {
+    theme: {},
+  },
+}
+
+export default FrontityWpTheme;
+
+
